26f65dcf86498aa345fd263eb9b2bda263648a12
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-05 16:45:04 +0200

    remove Save method from editor asset registry

--- Editor/Assets/LunyAssetRegistryManager.cs
+++ Editor/Assets/LunyAssetRegistryManager.cs
@@ -87,7 +87,6 @@ namespace CodeSmileEditor.Luny
 			{
 				var editorRegistry = LunyEditorAssetRegistry.instance;
 				FindAndRegisterAllLuaAssets(editorRegistry.LuaAssets, typeof(LunyEditorLuaAsset));
-				editorRegistry.Save();
 			}
 		}
 
--- Editor/Assets/LunyEditorAssetRegistry.cs
+++ Editor/Assets/LunyEditorAssetRegistry.cs
@@ -22,10 +22,5 @@ namespace CodeSmileEditor.Luny
 
 			return index >= 0 ? (LunyEditorLuaAsset)m_LuaAssets.Assets[index] : null;
 		}
-
-		internal void Save()
-		{
-			Save(true);
-		}
 	}
 }
