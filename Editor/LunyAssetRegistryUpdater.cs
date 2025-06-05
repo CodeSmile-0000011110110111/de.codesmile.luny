3f58654cd227066c3ce6a5fc6d42064318d90436
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-05 12:10:45 +0200

    refactor LunyAssetRegistry, editor code is now a ScriptableSingleton, changed updating to individual assets with AssetModificationProcessor

---
+++ Editor/LunyAssetRegistryUpdater.cs
@@ -0,0 +1,224 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using CodeSmile.Luny;
+using Lua.Unity;
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmileEditor.Luny
+{
+	internal sealed class LunyAssetRegistryUpdater : ScriptableSingleton<LunyAssetRegistryUpdater>
+	{
+		// delayCall prevents "Asset import worker (4)" warning spam when selecting the registry asset
+		// this is likely due to modifying the AssetDatabase 'too early' (ie same issue as with static ctor)
+		[InitializeOnLoadMethod] private static void OnLoad() => EditorApplication.delayCall += () => InitRegistry();
+		[InitializeOnEnterPlayMode] private static void OnEnterPlayMode() => InitRegistry();
+
+		private static void InitRegistry()
+		{
+			if (LunyAssetRegistry.Singleton == null)
+				LunyAssetRegistry.Singleton = FindOrCreateRegistry();
+
+			RegisterAllLunyAssets();
+		}
+
+		private static LunyAssetRegistry FindOrCreateRegistry()
+		{
+			var registryGuids = AssetDatabase.FindAssets($"t:{nameof(LunyAssetRegistry)}");
+
+			// we got multiple => delete all and create new one
+			if (registryGuids.Length > 1)
+			{
+				TryDeleteAllRegistries(registryGuids);
+				registryGuids = new String[0];
+			}
+
+			// we found none => create it
+			if (registryGuids.Length == 0)
+				return CreateRegistry();
+
+			var assetPath = AssetDatabase.GUIDToAssetPath(registryGuids[0]);
+			return AssetDatabase.LoadAssetAtPath<LunyAssetRegistry>(assetPath);
+		}
+
+		private static LunyAssetRegistry CreateRegistry()
+		{
+			var registry = CreateInstance<LunyAssetRegistry>();
+			var path = $"Assets/{nameof(LunyAssetRegistry)}.asset";
+			AssetDatabase.CreateAsset(registry, path);
+			return registry;
+		}
+
+		private static void TryDeleteAllRegistries(String[] registryAssetGuids)
+		{
+			LunyAssetRegistry.Singleton = null;
+
+			foreach (var registryGuid in registryAssetGuids)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(registryGuid);
+				var loadedRegistry = AssetDatabase.LoadAssetAtPath<LunyAssetRegistry>(path);
+				if (loadedRegistry != null)
+				{
+					// delete extraneous registry assets, no matter how they came to be (they would all be the same)
+					Debug.Log($"Deleting extraneous {nameof(LunyAssetRegistry)}: {path}");
+					AssetDatabase.DeleteAsset(path);
+				}
+			}
+		}
+
+		private static void RegisterAllLunyAssets()
+		{
+			Debug.LogWarning("TODO: scan for contexts");
+			var registry = LunyAssetRegistry.Singleton;
+			// ScanForLuaContexts();
+			FindAndRegisterAllLuaAssets(registry);
+			EditorUtility.SetDirty(registry);
+			AssetDatabase.SaveAssetIfDirty(registry);
+		}
+
+		private static void FindAndRegisterAllLuaAssets(LunyAssetRegistry registry)
+		{
+			registry.ClearLuaAssets();
+
+			var luaAssetGuids = AssetDatabase.FindAssets($"t:{nameof(LuaAsset)}");
+			var luaAssetCount = luaAssetGuids.Length;
+			for (var i = 0; i < luaAssetCount; i++)
+			{
+				var assetPath = AssetDatabase.GUIDToAssetPath(luaAssetGuids[i]);
+				var luaAsset = AssetDatabase.LoadAssetAtPath<LuaAsset>(assetPath);
+				if (luaAsset != null)
+					registry.AddLuaAsset(luaAsset, luaAsset.name, assetPath);
+			}
+		}
+
+		//[SerializeField] [ReadOnlyField] private String m_ModuleBindingsUnityVersion;
+		// private void Awake()
+		// {
+		// 	// Note: Awake runs when the asset is created OR when the project loads
+		// 	if (m_ModuleBindingsUnityVersion != Application.unityVersion)
+		// 		RegenerateAllModules();
+		// }
+		//
+		// private void RegenerateAllModules() => Debug.LogWarning("TODO: RegenerateAllModules");
+		// Debug.Log("Unity version has changed. Regenerating all LuaModule bindings ...");
+		// var moduleGuids = AssetDatabase.FindAssets($"t:{nameof(LuaModule)}");
+		//
+		// // use a temporary Lua context with no modules loaded
+		// var luaContext = CreateInstance<LuaContext>();
+		// var lua = luaContext.CreateLuaInstance(false);
+		//
+		// foreach (var moduleGuid in moduleGuids)
+		// {
+		// 	var path = AssetDatabase.GUIDToAssetPath(moduleGuid);
+		// 	var module = AssetDatabase.LoadAssetAtPath<LuaModule>(path);
+		// 	if (module != null)
+		// 		module.GenerateBindings(lua);
+		// }
+		//
+		// m_ModuleBindingsUnityVersion = Application.unityVersion;
+		// EditorUtility.SetDirty(this);
+		// AssetDatabase.SaveAssetIfDirty(this);
+		/*
+		private void ScanForLuaContexts()
+		{
+			m_LuaContexts.Clear();
+			m_DefaultContext = null;
+			m_DefaultEditorContext = null;
+
+			var luaContextGuids = AssetDatabase.FindAssets($"t:{nameof(LuaContext)}");
+			var luaContextCount = luaContextGuids.Length;
+			for (var i = 0; i < luaContextCount; i++)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(luaContextGuids[i]);
+				var luaContext = AssetDatabase.LoadAssetAtPath<LuaContext>(path);
+				if (luaContext != null)
+				{
+					m_LuaContexts.Add(luaContext);
+
+					if (luaContext.IsDefaultContext)
+					{
+						if (luaContext.IsEditorContext)
+						{
+							Debug.Assert(DefaultEditorContext == null, $"duplicate default editor LuaContext: {luaContext}");
+							m_DefaultEditorContext = luaContext;
+						}
+						else
+						{
+							Debug.Assert(DefaultContext == null, $"duplicate default runtime LuaContext: {luaContext}");
+							m_DefaultContext = luaContext;
+						}
+					}
+				}
+			}
+
+			Debug.Assert(DefaultContext != null, "Missing default runtime LuaContext!");
+			Debug.Assert(DefaultEditorContext != null, "Missing default editor LuaContext!");
+		}
+		*/
+
+		private class LuaAssetModificationProcessor : AssetModificationProcessor
+		{
+			private static void OnWillCreateAsset(String assetPath)
+			{
+				if (IsLuaAsset(assetPath))
+				{
+					// need to delay since asset cannot be loaded within this callback
+					EditorApplication.delayCall += () =>
+					{
+						var luaAsset = AssetDatabase.LoadAssetAtPath<LuaAsset>(assetPath);
+						if (luaAsset != null)
+						{
+							if (LunyAssetRegistry.Singleton == null)
+								InitRegistry();
+							else
+								LunyAssetRegistry.Singleton.AddLuaAsset(luaAsset, luaAsset.name, assetPath);
+						}
+					};
+				}
+			}
+
+			private static AssetDeleteResult OnWillDeleteAsset(String assetPath, RemoveAssetOptions options)
+			{
+				if (IsLuaAsset(assetPath))
+				{
+					var luaAsset = AssetDatabase.LoadAssetAtPath<LuaAsset>(assetPath);
+					if (luaAsset != null)
+					{
+						if (LunyAssetRegistry.Singleton == null)
+							EditorApplication.delayCall += InitRegistry; // delay to ensure asset is already deleted
+						else
+							LunyAssetRegistry.Singleton.RemoveLuaAsset(luaAsset, luaAsset.name, assetPath);
+					}
+				}
+				return AssetDeleteResult.DidNotDelete;
+			}
+
+			private static AssetMoveResult OnWillMoveAsset(String sourcePath, String destinationPath)
+			{
+				if (IsLuaAsset(sourcePath))
+				{
+					var luaAsset = AssetDatabase.LoadAssetAtPath<LuaAsset>(sourcePath);
+					if (luaAsset != null)
+					{
+						if (LunyAssetRegistry.Singleton == null)
+							EditorApplication.delayCall += InitRegistry; // delay to ensure asset was already moved
+						else
+						{
+							LunyAssetRegistry.Singleton.RemoveLuaAsset(luaAsset, luaAsset.name, sourcePath);
+
+							// asset instance still has the old name so use the name from path instead
+							var newName = Path.GetFileNameWithoutExtension(destinationPath);
+							LunyAssetRegistry.Singleton.AddLuaAsset(luaAsset, newName, destinationPath);
+						}
+					}
+				}
+				return AssetMoveResult.DidNotMove;
+			}
+
+			private static Boolean IsLuaAsset(String assetPath) => Path.GetExtension(assetPath) == ".lua";
+		}
+	}
+}
--- Runtime/Assets/LunyAssetRegistry.cs
+++ Runtime/Assets/LunyAssetRegistry.cs
@@ -13,32 +13,52 @@ namespace CodeSmile.Luny
 	/// <summary>
 	/// Maintains a list of project-wide LuaAsset (*.lua) assets in support of 'load bundled script by name/path'.
 	/// </summary>
-	public sealed partial class LunyAssetRegistry : ScriptableObject
+	public sealed class LunyAssetRegistry : ScriptableObject
 	{
 		private static LunyAssetRegistry s_Singleton;
 
-		[Header("Automated Luny Support Asset (read-only)")]
+		[Header("Fully automated and required Asset (read-only)")]
 		[SerializeField] [ReadOnlyField] private LuaContext m_DefaultContext;
-		[SerializeField] [ReadOnlyField] private LuaContext m_DefaultEditorContext;
 		[SerializeField] [ReadOnlyField] private List<LuaContext> m_LuaContexts = new();
 		[SerializeField] [ReadOnlyField] private List<LuaAsset> m_LuaAssets = new();
 		[SerializeField] [ReadOnlyField] private List<String> m_LuaAssetNames = new();
-		[SerializeField] [ReadOnlyField] private List<String> m_LuaAssetNamesLowercase = new();
 		[SerializeField] [ReadOnlyField] private List<String> m_LuaAssetPaths = new();
-		public static LunyAssetRegistry Singleton => s_Singleton;
+
+		public static LunyAssetRegistry Singleton { get => s_Singleton; internal set => s_Singleton = value; }
 		public LuaContext DefaultContext => m_DefaultContext;
-		public LuaContext DefaultEditorContext => m_DefaultEditorContext;
+
+		private void Awake() => s_Singleton = this;
+
 		public LuaContext GetContext(String contextName) => m_LuaContexts.Find(context => context.name == contextName);
 
 		public LuaAsset GetScript(String scriptName)
 		{
-			var index = m_LuaAssetNamesLowercase.IndexOf(scriptName.ToLower());
-			if (index < 0)
-				index = m_LuaAssetNames.IndexOf(scriptName);
+			var index = m_LuaAssetNames.IndexOf(scriptName);
 			if (index < 0)
 				index = m_LuaAssetPaths.IndexOf(scriptName);
 
 			return index >= 0 ? m_LuaAssets[index] : null;
+		}
+
+		internal void AddLuaAsset(LuaAsset luaAsset, string assetName, String assetPath)
+		{
+			m_LuaAssets.Add(luaAsset);
+			m_LuaAssetNames.Add(assetName);
+			m_LuaAssetPaths.Add(assetPath);
+		}
+
+		internal void RemoveLuaAsset(LuaAsset luaAsset, string assetName, String assetPath)
+		{
+			m_LuaAssets.Remove(luaAsset);
+			m_LuaAssetNames.Remove(assetName);
+			m_LuaAssetPaths.Remove(assetPath);
+		}
+
+		internal void ClearLuaAssets()
+		{
+			m_LuaAssets.Clear();
+			m_LuaAssetNames.Clear();
+			m_LuaAssetPaths.Clear();
 		}
 	}
 }
