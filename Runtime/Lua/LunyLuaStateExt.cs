b223b4917776b8da6022dd7a61ce9f50b2f7ecec
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-20 11:31:07 +0200

    rename extension class

--- Runtime/Lua/LunyLuaStateExt.cs
+++ Runtime/Lua/LunyLuaStateExt.cs
@@ -10,20 +10,19 @@ using UnityEngine;
 
 namespace CodeSmile.Luny
 {
-	public static class LunyLuaOverrides
+	public static class LunyLuaStateExt
 	{
 		public static async ValueTask<LuaClosure> LunyLoadFileAsync(this LuaState state, string fileName, LuaTable environment, CancellationToken cancellationToken)
 		{
 			using var stream = await state.FileSystem.Open(fileName, LuaFileOpenMode.Read, cancellationToken);
 			var source = await stream.ReadAllAsync(cancellationToken);
 
+			// get the relative asset path, if any, and use it as the chunk name for hyperlinking
 			var fileSystem = state.Platform.FileSystem as LunyLuaFileSystem;
 			var assetPath = fileSystem.Hook.TryGetAssetPath(fileName);
-			Debug.LogWarning($"load: {fileName} => {assetPath}");
 			if (assetPath != null)
 				fileName = assetPath;
 
-			//var chunkName = "@" + fileName;
 			LuaClosure closure = state.Load(source, fileName, environment);
 			return closure;
 		}
